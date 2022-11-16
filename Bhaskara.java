2916fde597429678bb7f194af9cba079adc82071
Marco Mangan <marco.mangan@pucrs.br>
2022-11-16 10:26:02 -0300

    Criei atributos para armazenar os parâmetros da equação.

--- Bhaskara.java
+++ Bhaskara.java
@@ -7,27 +7,15 @@
  */
 public class Bhaskara
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
+    private double a;
+    private double b;
+    private double c;
 
     /**
      * Construtor para objetos da classe Bhaskara
      */
     public Bhaskara()
     {
-        // inicializa variáveis de instância
-        x = 0;
     }
 
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
-    }
 }
