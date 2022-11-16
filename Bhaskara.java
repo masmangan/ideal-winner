4f8025de9ffe2cc212df6d17549f7b3be377e310
Marco Mangan <marco.mangan@pucrs.br>
2022-11-16 10:40:56 -0300

    Métodos essencias: entradas e saídas!

--- Bhaskara.java
+++ Bhaskara.java
@@ -14,8 +14,25 @@ public class Bhaskara
     /**
      * Construtor para objetos da classe Bhaskara
      */
-    public Bhaskara()
+    public Bhaskara(double a, double b, double c)
     {
+        if (a == 0.0)
+            throw new IllegalArgumentException("A=0! Nao e um equacao do segundo grau!");
+            
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    
+    
+    public double raiz1() {
+        //TODO:
+        return 0.0;
     }
 
+    public double raiz2() {
+        //TODO:
+        return 0.0;
+    }    
+    
 }
